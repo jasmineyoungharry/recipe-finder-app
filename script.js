0308c060a2465868cabe3066bb39f0ca91665d0d
Jasmine Youngharry <jazzyyoungharr@gmail.com>
2026-05-07 22:58:33 -0600

    Connect app to TheMealDB API using fetch and async await

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Recipe Finder App Loaded");
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+
+async function searchRecipes() {
+
+    const searchValue = searchInput.value.trim();
+
+    if (searchValue === "") {
+        alert("Please enter a meal name.");
+        return;
+    }
+
+    const apiUrl =
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
+
+    try {
+
+        const response = await fetch(apiUrl);
+
+        const data = await response.json();
+
+        console.log(data);
+
+    } catch (error) {
+
+        console.log("Error fetching recipes:", error);
+
+    }
+}
+
+searchButton.addEventListener("click", searchRecipes);
